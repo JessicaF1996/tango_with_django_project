28e84dc234d3bb3f5260232baa692f6c8f7a1cc6
Jessica Fong <2138525F@student.gla.ac.uk>
2019-01-22 15:34:18 +0000

    chapter 5 completed populate_rango script

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
 ]
 
 MIDDLEWARE = [
