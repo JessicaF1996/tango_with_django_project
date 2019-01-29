fe0de648446f01a99766e3d54dd0a066c6b4d815
Jessica Fong <2138525F@student.gla.ac.uk>
2019-01-29 18:10:07 +0000

    finished exercise and chapter 9

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
 from rango.forms import UserForm, UserProfileForm
-
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 def add_category(request):
@@ -91,6 +94,37 @@ def register(request):
 		user_form = UserForm()
 		profile_form = UserProfileForm()
 
-	return render(request, 'rango/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+	return render(request, 'rango/register.html', 
+		{'user_form': user_form, 
+		'profile_form': profile_form, 
+		'registered': registered})
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect(reverse('index'))
+			else:
+				return HttpResponse("Your Rango account is disabled,")
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password))
+			return HttpResponse("Invalid login details supplied. Please go back to the login page")
+
+	else:
+		return render(request, 'rango/login.html', {})
+
+@login_required
+def restricted(request):
+	return render(request, 'rango/restricted.html', {})
+
+@login_required
+def user_logout(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('index'))
+
 
 
